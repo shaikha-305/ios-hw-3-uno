a5ef8a0eab7b71703f04b54047fe7892dc57065e
shaikha-305 <shaikhaaljenaidel@gmail.com>
2020-09-21 01:51:03 +0300

    done with enums

--- iOS-HW-3-Uno.playground/Contents.swift
+++ iOS-HW-3-Uno.playground/Contents.swift
@@ -88,60 +88,121 @@ var wild_Draw = UIImage(named: "Wild_Draw.png")
 /// قم بإنشاء الستركت هنا
 
 // struct ...
-struct Card
-{
-    var color: String?
-    var number: Int?
-    var action: String?
+//struct Card
+//{
+//    var color: String?
+//    var number: Int?
+//    var action: String?
+//
+//    func imageName() -> String
+//    {
+//        if color != "Wild"  && number != nil && action == nil
+//       {
+//            return "\(color!)_\(number!)"
+//        } else if color != "Wild" && number == nil && action != nil {
+//            return "\(color!)_\(action!)"
+//        } else if color == "Wild" && number == nil && action != nil {
+//            return "\(action!)"
+//       }
+//        return ""
+//    }
+//}
+//
+//var colors = ["Blue", "Green", "Red", "Yellow", "Wild"]
+//var actions = ["Reverse", "Skip", "Draw"]
+//var wildActions = ["Wild", "Wild_Draw"]
+//var cards: [Card] = []
+//for color in colors
+//{
+//    if color != "Wild" {
+//    for num in 0...9
+//    {
+//
+//        var sampleCard = Card(color: color, number: num)
+//
+//        cards.append(sampleCard)
+//        if sampleCard.number != 0{
+//            cards.append(sampleCard)
+//        }
+//
+//    }
+//    for action1 in actions{
+//        var sampleCard = Card(color: color, action: action1)
+//        cards.append(sampleCard)
+//    }
+//    } else if color == "Wild" {
+//        for action in wildActions {
+//            var wildCards = Card(color: "Wild", action: action)
+//            cards.append(wildCards)
+//            cards.append(wildCards)
+//            cards.append(wildCards)
+//            cards.append(wildCards)
+//        }
+//    }
+//}
+
+enum Card{
+    case number(Color, Int)
+    case action(Color, Action)
+    case wild(Wild)
     
-    func imageName() -> String
-    {
-        if color != "Wild"  && number != nil && action == nil
-       {
-            return "\(color!)_\(number!)"
-        } else if color != "Wild" && number == nil && action != nil {
-            return "\(color!)_\(action!)"
-        } else if color == "Wild" && number == nil && action != nil {
-            return "\(action!)"
-       }
-        return ""
+    func image() -> String{
+        switch self{
+        case .number(let color, let number):
+            return "\(color)_\(number)"
+        case .action(let color, let action):
+            return "\(color)_\(action)"
+        case .wild(let wild):
+            return "\(wild)"
+        }
     }
 }
+enum Color: CaseIterable{
+    case Red, Blue, Green, Yellow
+}
 
-var colors = ["Blue", "Green", "Red", "Yellow", "Wild"]
-var actions = ["Reverse", "Skip", "Draw"]
-var wildActions = ["Wild", "Wild_Draw"]
+enum Action: CaseIterable{
+    case Skip, Draw, Reverse
+}
+
+enum Wild: CaseIterable{
+    case Wild, Wild_Draw
+}
+
 var cards: [Card] = []
-for color in colors
-{
-    if color != "Wild" {
-    for num in 0...9
-    {
-        
-        var sampleCard = Card(color: color, number: num)
-        
-        cards.append(sampleCard)
-        if sampleCard.number != 0{
-            cards.append(sampleCard)
-        }
-        
+
+
+for color in Color.allCases{
+    for i in 0...9{
+        let card = Card.number(color, i)
+        cards.append(card)
     }
-    for action1 in actions{
-        var sampleCard = Card(color: color, action: action1)
-        cards.append(sampleCard)
+    for action in Action.allCases{
+        let card = Card.action(color, action)
+        cards.append(card)
     }
-    } else if color == "Wild" {
-        for action in wildActions {
-            var wildCards = Card(color: "Wild", action: action)
-            cards.append(wildCards)
-            cards.append(wildCards)
-            cards.append(wildCards)
-            cards.append(wildCards)
-        }
+    for i in 1...9{
+       let card = Card.number(color, i)
+        cards.append(card)
+    }
+    for action in Action.allCases{
+        let card = Card.action(color, action)
+        cards.append(card)
+    }
+    
+    for wild in Wild.allCases{
+        let card = Card.wild(wild)
+        cards.append(card)
     }
 }
 
- 
+
+
+let randomCard = cards.randomElement()!
+let randomCardImage = UIImage(named: randomCard.image())
+
+let cardImages = cards.map{UIImage(named: $0.image())}
+cardImages
 
 
 
@@ -152,14 +213,31 @@ for color in colors
 // لا تقم بإزالة الملاحظات إلا عند وصولك للمطلوب الثالث
 
 
-let randomCard = cards.randomElement()!
-let randomCardImage = UIImage(named: randomCard.imageName())
+//let randomCard = cards.randomElement()!
+//let randomCardImage = UIImage(named: randomCard.imageName())
+//
+//
+//let cardImages = cards.map{UIImage(named: $0.imageName())}
+//randomCardImage
+//cardImages
 
 
-let cardImages = cards.map{UIImage(named: $0.imageName())}
-randomCardImage
-cardImages
 
+struct Movie{
+    var title: String
+    var pgRate: String
+    var description: String
+    
+    func printDescription(){
+        if pgRate == "PG"{
+            print("movie is suitable for kids, \(description)")
+        }else if pgRate == "PG-13"{
+            print("movie is not suitable for kids, \(description)")
+        }
+    }
+}
+ var harryPotter = Movie(title: "Harry Poter", pgRate: "PG", description: "movie")
 
-
-
+for color in ["yellow", "green", "blue"]{
+    print(color)
+}
